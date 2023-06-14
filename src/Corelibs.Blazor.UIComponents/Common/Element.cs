c3c6e4bd23813b6085a5eca9b1922308ec6be4ef
drlsn <dariusz.lusnia@gmail.com>
2023-06-14 23:16:39 +0200

    Improve dropdown, list, item

---
+++ src/Corelibs.Blazor.UIComponents/Common/Element.cs
@@ -0,0 +1,13 @@
+﻿namespace Corelibs.Blazor.UIComponents.Common;
+
+public record Element(string Id, string Name, string Value, List<Element> Children)
+{
+
+}
+
+public record ElementData(string Id, string Name, string Value);
+
+public record Attribute(string Name, string value)
+{
+
+}
---
+++ src/Corelibs.Blazor.UIComponents/Common/StringExtensions.cs
@@ -0,0 +1,6 @@
+﻿namespace Corelibs.Blazor.UIComponents.Common;
+
+public static class StringExtensions
+{
+    public static bool IsOk(this string str) => !string.IsNullOrWhiteSpace(str);
+}
