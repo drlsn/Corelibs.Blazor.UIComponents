0b56f5e5e09a6aa72acccc225a5b661f3b0d3f53
drlsn <dariusz.lusnia@gmail.com>
2023-06-16 22:50:32 +0200

    Add base layout

---
+++ src/Corelibs.Blazor.UIComponents/Layouts/BaseLayout.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Corelibs.Blazor.UIComponents.Layouts;
+
+public class BaseLayout : LayoutComponentBase
+{
+    private int _menuSize;
+    protected string _paddingLeftCssAttribute => _menuSize == 0 ? "" : $"padding-left: {_menuSize.ToString()}px;";
+
+    protected void OnMenuSizeChanged(int size)
+    {
+        _menuSize = size;
+        InvokeAsync(StateHasChanged);
+    }
+}
